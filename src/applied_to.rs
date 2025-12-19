a1396d383490855edf1534a5a6de958103035020
Eduardo R. D'Avila <erdavila@gmail.com>
2025-12-19 12:41:15 -0300

    Add doc to AppliedTo

--- src/applied_to.rs
+++ src/applied_to.rs
@@ -2,7 +2,9 @@ use core::fmt::Display;
 
 use crate::{Format, Formatted, ToFormat, ToFormatSet};
 
+/// A trait to apply a formatting to some content.
 pub trait AppliedTo: ToFormat + ToFormatSet<FormatSet = Format> {
+    /// Applies the formatting to the given content, returning a [`Formatted<C>`](Formatted) instance.
     #[must_use]
     fn applied_to<C: Display>(self, content: C) -> Formatted<C> {
         self.to_format().applied_to(content)
