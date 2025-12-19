588b0d8d7cd7534a946a3eaf41b247320954090e
Eduardo R. D'Avila <erdavila@gmail.com>
2025-12-19 12:44:38 -0300

    Add doc to ToFormat

--- src/to_format.rs
+++ src/to_format.rs
@@ -1,6 +1,9 @@
+#![warn(missing_docs)]
 use crate::Format;
 
+/// A trait to convert a type into a [`Format`].
 pub trait ToFormat: Into<Format> {
+    /// Converts the type into a [`Format`].
     #[must_use]
     fn to_format(self) -> Format;
 }
